15695ac1c9d6fa633a16939d3d68d8f8b0143253
Astra Bertelli <astraberte9@gmail.com>
2024-12-20 16:35:46 +0100

    minor update

--- scripts/SyncToVcfField.py
+++ scripts/SyncToVcfField.py
@@ -10,26 +10,26 @@ def read_sync(syncfile: str) -> pl.DataFrame:
     return df
 
 def add_dros_sim(vcfdf: pl.DataFrame, syncdf: pl.DataFrame):
-    vcfdf = vcfdf.rename({"#CHROM": "CHROM"})
-    findf = syncdf.join(vcfdf, on=["CHROM", "POS"], how="inner")
+    syncdf = syncdf.rename({"CHROM": "#CHROM"})
+    findf = syncdf.join(vcfdf, on=["#CHROM", "POS"], how="inner")
     print("Perfomerd join")
     finaldf = findf.drop(findf.columns[4:])
     ls = []
     positionsvcf = vcfdf["POS"].to_list()
-    chromsvcf = vcfdf["CHROM"].to_list() 
+    chromsvcf = vcfdf["#CHROM"].to_list() 
     refallelesvcf = vcfdf["REF"].to_list()
     altallelesvcf = vcfdf["ALT"].to_list() 
     vcf = {f"{chromsvcf[i]}.{positionsvcf[i]}": [refallelesvcf[i], altallelesvcf[i]] for i in range(len(positionsvcf))}  
-    syncchrom = finaldf["CHROM"].to_list() 
+    syncchrom = finaldf["#CHROM"].to_list() 
     syncpos = finaldf["POS"].to_list() 
     syncalleles = finaldf["ALL"].to_list() 
     sync = {f"{syncchrom[i]}.{syncpos[i]}": syncalleles[i] for i in range(len(syncpos))} 
     print("Created dicts")
     for k in vcf:
         if sync[k] == vcf[k][0]:
-            ls.append("0/0:1,1,1:1:1,0:1,0,0:10")
+            ls.append("0/0:1,1,1:100:100,0:1,0,0:10")
         elif sync[k] == vcf[k][1]:
-            ls.append("1/1:1,1,1:1:0,1:1,0,0:10")
+            ls.append("1/1:1,1,1:100:0,100:1,0,0:10")
         else:
             ls.append("./.:0,0,0:0:0,0:0,0,0:0") 
     print("Finished loop")
