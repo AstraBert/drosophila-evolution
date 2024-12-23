c18c16620d28a850e6f1efbad923812aabd5b29d
Astra Clelia Bertelli <astraberte9@gmail.com>
2024-12-23 07:06:53 +0100

    23rd december🎅

---
+++ scripts/Vcf2DiyabcGen.r
@@ -0,0 +1,23 @@
+require(poolfstat)
+
+# script.R
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+    stop("At least one argument must be supplied", call.=FALSE)
+}
+
+# Access arguments by index
+vcf_file <- args[1]
+writing_dir <- args[2]
+
+vcfdata <- vcf2pooldata(vcf.file=vcf_file, min.maf="0.05", poolsizes=rep(50,4))
+
+pooldata2diyabc(
+    vcfdata,
+    writing.dir = writing_dir,
+    prefix = "PopData",
+    diyabc.mrc = 1,
+)
+
+
