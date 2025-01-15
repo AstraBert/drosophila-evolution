88a0ab40c9620a4ae98c914589f219898b62132a
Clelia (Astra) Bertelli <astraberte9@gmail.com>
2025-01-15 15:56:23 +0100

    sim changes

--- shell/generate_diyabc_inputs.sh
+++ shell/generate_diyabc_inputs.sh
@@ -1,25 +1,18 @@
 wd="/home/abert/media1/projects/drosophila-evolution"
 vcf_file="$wd/results/drosophila_evolution.bcftools_fakepools_wholegen.vcf.gz"
 
-mkdir -p $wd/data/simulation_vcfs/
 mkdir -p $wd/data/simulation_data/logs/
 
 for i in {1..4}
 do
-	conda activate gatk_modified
-    bcftools view \
-        -S $wd/data/simulation_data/subsample_${i}.txt \
-        -O z \
-        -o $wd/data/simulation_vcfs/subsample_${i}.vcf.gz \
-        $vcf_file 2> $wd/data/simulation_data/logs/bcftools.log
-	conda deactivate
 
+   	input_vcf="$wd/data/simulation_vcfs/subsample_${i}.vcf.gz"
     mkdir -p $wd/data/diyabc_inputs_${i}/
 
-    conda activate R
+    source activate R
 
     Rscript $wd/scripts/r/Vcf2DiyabcGen.r \
-        $wd/data/simulation_vcfs/subsample_${i}.vcf.gz \
+        $input_vcf \
         $wd/data/diyabc_inputs_${i}/
     
     conda deactivate
