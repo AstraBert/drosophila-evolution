2de738b568b268013610dc4b41d203204a08c2f5
Astra Clelia Bertelli <astraberte9@gmail.com>
2025-01-16 14:06:09 +0100

    DrosSim fk

--- scripts/python/DrosevolVcf2Alleles.py
+++ scripts/python/DrosevolVcf2Alleles.py
@@ -5,6 +5,9 @@ def read_vcf(vcffile: str) -> pl.DataFrame:
     return df
 
 if __name__ == "__main__":
+    sync = pl.read_csv("dros_sim.sync.gz", separator="\t")
+    sync = sync.rename({"CHROM": "Chromosome", "POS": "Position", "ALL": "DrosSim"})
+    sync = sync.drop("COUNT")
     vcf = read_vcf("drosevol.noneurope.vcf.gz")
     vcf = vcf.rename({"#CHROM": "Chromosome", "POS": "Position", "REF": "RefAllele", "ALT": "AltAllele"})
     vcf = vcf.drop(["ID", "QUAL", "FILTER", "INFO", "FORMAT"])
@@ -12,41 +15,50 @@ if __name__ == "__main__":
     df = pl.read_csv("dest_eu_snps.tsv", separator="\t")
     print(df.head())
     dest_dros = vcf.join(df, on=["Chromosome", "Position", "RefAllele", "AltAllele"], how="inner")
-    selected_snps = dest_dros["id"].to_list()
+    dest_dros_sim = dest_dros.join(sync, on=["Chromosome", "Position"], how="inner") 
+    selected_snps = dest_dros_sim["id"].to_list()
     t = open("selected_dest_eu_snps.csv", "w")
     t.write("POS\n")
     selected_snps = [str(snp)+"\n" for snp in selected_snps]
     t.writelines(selected_snps)
     t.close()
-    dgn = dest_dros["DGN"].to_list()
+    drossim = dest_dros_sim["DrosSim"].to_list()
+    refalleles = dest_dros_sim["RefAllele"].to_list()
+    altalleles = dest_dros_sim["AltAllele"].to_list()
+    drossim_allelic_status = [
+        0 if drossim[i] == altalleles[i]
+        else 100
+        for i in range(len(drossim))
+    ]
+    dgn = dest_dros_sim["DGN"].to_list()
     dgn_allelic_status = [
         0 if el.split(":")[0] == "1/1" or el.split(":")[0] == "./."
         else 20 if el.split(":")[0] == "0/1" or el.split(":")[0] == "1/0"
         else 40
         for el in dgn
     ]
-    cnxj = dest_dros["CNXJ"].to_list()
+    cnxj = dest_dros_sim["CNXJ"].to_list()
     cnxj_allelic_status = [
         0 if el.split(":")[0] == "1/1" or el.split(":")[0] == "./."
         else 13 if el.split(":")[0] == "0/1" or el.split(":")[0] == "1/0"
         else 25
         for el in cnxj
     ]
-    cnother = dest_dros["CnOther"].to_list()
+    cnother = dest_dros_sim["CnOther"].to_list()
     cnother_allelic_status = [
         0 if el.split(":")[0] == "1/1" or el.split(":")[0] == "./."
         else 25 if el.split(":")[0] == "0/1" or el.split(":")[0] == "1/0"
         else 50
         for el in cnother
     ]
-    cnqtp = dest_dros["CnQTP"].to_list()
+    cnqtp = dest_dros_sim["CnQTP"].to_list()
     cnqtp_allelic_status = [
         0 if el.split(":")[0] == "1/1" or el.split(":")[0] == "./."
         else 25 if el.split(":")[0] == "0/1" or el.split(":")[0] == "1/0"
         else 50
         for el in cnqtp
     ]
-    isr = dest_dros["ISR"].to_list()
+    isr = dest_dros_sim["ISR"].to_list()
     isr_allelic_status = [
         0 if el.split(":")[0] == "1/1" or el.split(":")[0] == "./."
         else 16 if el.split(":")[0] == "0/1" or el.split(":")[0] == "1/0"
@@ -54,8 +66,8 @@ if __name__ == "__main__":
         for el in cnqtp
     ]
     f = open("drosevol_readcount.csv", "w")
-    f.write("DGN,CNXJ,CnOther,CnQTP,ISR\n")
+    f.write("DGN,CNXJ,CnOther,CnQTP,ISR,DrosSim\n")
     for el in range(len(isr_allelic_status)):
-        f.write(f"{dgn_allelic_status[el]},{cnxj_allelic_status[el]},{cnother_allelic_status[el]},{cnqtp_allelic_status[el]},{isr_allelic_status[el]}\n")
+        f.write(f"{dgn_allelic_status[el]},{cnxj_allelic_status[el]},{cnother_allelic_status[el]},{cnqtp_allelic_status[el]},{isr_allelic_status[el]},{drossim_allelic_status[el]}\n")
     f.close()
     
